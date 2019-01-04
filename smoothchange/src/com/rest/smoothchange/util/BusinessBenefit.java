53c0e23486c57cde2b647b6c52842b1e90dc3a19
shibajichatterjee <40158119+shibajichatterjee@users.noreply.github.com>
2019-01-04 19:30:32 +0530

    Update BusinessBenefit.java

--- smoothchange/src/com/rest/smoothchange/util/BusinessBenefit.java
+++ smoothchange/src/com/rest/smoothchange/util/BusinessBenefit.java
@@ -2,9 +2,9 @@ package com.rest.smoothchange.util;
 
 public enum BusinessBenefit {
 
-	REDUCECOST("Reduce Cost"),INCREASEREVENUE("Increase Revenue"),INCREACECUSTOMERBASE("Increace Customer Base"),
-	IMPROVEPROCESS("Improve Process"), IMPROVEPRODUCTIVITY("Improve Productivity"),ENHANCEUSEREXPERIANCE("Enhance User Experiance"),
-	INCREASECUSTOMERSATISFICTION("Increase Customer Satisfiction"),OTHER("Other");
+	REDUCECOST("Reduce Cost"),INCREASEREVENUE("Increase Revenue"),INCREACECUSTOMERBASE("Increase Customer Base"),
+	IMPROVEPROCESS("Process Improve"), IMPROVEPRODUCTIVITY("Improve Productivity"),ENHANCEUSEREXPERIANCE("Enhance User Experience"),
+	INCREASECUSTOMERSATISFICTION("Increase Customer Satisfaction"),OTHER("Other");
 	
 	private String value;
 	
